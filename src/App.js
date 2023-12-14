af4a99ded7613bc5f9bd2eff7e94ad2222412a85
kaushikaryan04 <kaushik.aryan04@gmail.com>
2023-12-15 03:37:56 +0530

    changed some files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import About from "./components/About"
-import Blog from "./components/Blog1"
-import Contactus from "./components/Contactus"
-import Footer from "./components/Footer";
-import Header from "./components/Header"
+
 import ProjectsPage from "./pages/ProjectsPage"
 import Home from './pages/Homepage'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
